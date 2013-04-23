9a94c316773fc64db8f35030117c8b4eecd1bfba
Nobuhisa HIRATA <wirapon55@gmail.com>
2013-04-23 13:18:03 +0900

    practice2 is completed.

--- practice/fundamentals/2nd/ActivityPractice/Practice1/src/jp/mixi/practice/activity/beg/UnkoActivity.java
+++ practice/fundamentals/2nd/ActivityPractice/Practice1/src/jp/mixi/practice/activity/beg/UnkoActivity.java
@@ -7,7 +7,7 @@ public class UnkoActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_unko);
     }
 
     @Override
