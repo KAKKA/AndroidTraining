c3154b5189acbecda7feafd54adbffb73e7bec6d
Nobuhisa HIRATA <wirapon55@gmail.com>
2013-05-14 17:21:30 +0900

    notification practice 1 and 2 are completed.

--- practice/fundamentals/4th/NotificationPractice1/src/jp/mixi/practice/notification/beg/MainActivity.java
+++ practice/fundamentals/4th/NotificationPractice1/src/jp/mixi/practice/notification/beg/MainActivity.java
@@ -2,7 +2,13 @@
 package jp.mixi.practice.notification.beg;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 
 public class MainActivity extends Activity {
 
@@ -12,5 +18,19 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         // TODO ここで通知を表示する
+        Intent intent = new Intent(this, SubActivity.class);
+
+        // PendingIntent オブジェクトの生成。このオブジェクトを他のアプリに渡すことで、引数に渡した Intent の送信を委ねることができる
+        // PendingIntent は、Intent の送信先のコンポーネントの種類によって使い分けること
+        PendingIntent activityIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        Notification notification = builder.setWhen(System.currentTimeMillis())
+                .setContentTitle("unko").setContentText("どぅえええ！？")
+                .setSmallIcon(R.drawable.ic_launcher).setTicker("おんどれあ　やんのかこらあ　ぶっ殺す！")
+                .setContentIntent(activityIntent).setOnlyAlertOnce(true)
+                .setAutoCancel(true).build();
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, notification);
     }
 }
