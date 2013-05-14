ed2a8b02f120e4f5746003fd6b6045e99f2f733b
Nobuhisa HIRATA <wirapon55@gmail.com>
2013-05-14 22:29:25 +0900

    ListView practice 1 and 2 are completed.

--- practice/fundamentals/6th/ListViewPractice/src/jp/mixi/practice/listview/beg/MainActivity.java
+++ practice/fundamentals/6th/ListViewPractice/src/jp/mixi/practice/listview/beg/MainActivity.java
@@ -1,18 +1,58 @@
 
 package jp.mixi.practice.listview.beg;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+
+    private MainActivity mActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        mActivity = this;
 
+        // ListViewに表示するデータを作成する
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < 20; i++) {
+            list.add("hoge" + i);
+        }
+
+        final ListView listView = (ListView) findViewById(R.id.ListView);
+        CustomListItemAdapter adapter = new CustomListItemAdapter(mActivity, list);
+        listView.setAdapter(adapter);
+        // リストアイテムをタップした時の動作を定義する
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Adapterからタップした位置のデータを取得する
+                String str = (String) parent.getItemAtPosition(position);
+                Toast.makeText(mActivity, str, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        View button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listView.smoothScrollToPosition(0);
+            }
+        });
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
