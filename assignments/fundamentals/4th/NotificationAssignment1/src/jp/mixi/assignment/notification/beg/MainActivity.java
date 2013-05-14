2c4e38ed55be0020b881b4d0d9525891793d96f7
Nobuhisa HIRATA <wirapon55@gmail.com>
2013-05-14 17:52:05 +0900

    notification practice 3 is completed.

--- assignments/fundamentals/4th/NotificationAssignment1/src/jp/mixi/assignment/notification/beg/MainActivity.java
+++ assignments/fundamentals/4th/NotificationAssignment1/src/jp/mixi/assignment/notification/beg/MainActivity.java
@@ -2,14 +2,48 @@
 package jp.mixi.assignment.notification.beg;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+
+import jp.mixi.assignment.*;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Intent intent = new Intent(this, SubActivity.class);
+        final PendingIntent activityIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
 
-        // TODO ここで通知を表示する
+        View button = findViewById(R.id.sikko);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO ここで通知を表示する
+
+                // PendingIntent オブジェクトの生成。このオブジェクトを他のアプリに渡すことで、引数に渡した Intent の送信を委ねることができる
+                // PendingIntent は、Intent の送信先のコンポーネントの種類によって使い分けること
+                Notification notification = builder.setWhen(System.currentTimeMillis())
+                        .setContentTitle("unko")
+                        .setContentText("どぅえええ！？")
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setTicker("おんどれあ　やんのかこらあ　ぶっ殺す！")
+                        .setContentIntent(activityIntent)
+                        .setOnlyAlertOnce(false)
+                        .setDefaults(Notification.DEFAULT_VIBRATE)
+                        .setAutoCancel(true)
+                        .build();
+
+                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(0, notification);
+            }
+        });
     }
 }
