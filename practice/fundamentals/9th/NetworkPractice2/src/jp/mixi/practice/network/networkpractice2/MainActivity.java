29ca9fc5789c1ba6f8c730b27127e0321a53ca93
Nobuhisa HIRATA <wirapon55@gmail.com>
2013-05-15 19:00:30 +0900

    Completet network practice 2.

--- practice/fundamentals/9th/NetworkPractice2/src/jp/mixi/practice/network/networkpractice2/MainActivity.java
+++ practice/fundamentals/9th/NetworkPractice2/src/jp/mixi/practice/network/networkpractice2/MainActivity.java
@@ -6,6 +6,21 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+
+import jp.mixi.practice.network.networkpractice2.R;
+//import org.apache.http.client.methods.HttpGet;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends Activity {
 
@@ -14,27 +29,52 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         View buttonGet = (Button)findViewById(R.id.buttonGet);
+
         buttonGet.setOnClickListener(new OnClickListener() {
 
             public void onClick(View v) {
                 // http getの処理を書く
-            }
-        });
-        
-        View buttonPost = findViewById(R.id.buttonPost);
-        buttonPost.setOnClickListener(new OnClickListener() {
+                EditText text = (EditText) findViewById(R.id.accessUrl);
+                HttpClient client = new DefaultHttpClient();
+                try {
+                    HttpGet httpGet = new HttpGet("http://mixi.jp");
+                    HttpResponse httpResponse = client.execute(httpGet);
+                    if(httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(httpResponse.getEntity().getContent())
+                                );
+                        StringBuilder response = new StringBuilder();
+                        String line;
+                        while((line = reader.readLine()) != null) {
+                            response.append(line);
+                        }
+                        reader.close();
+                        text.setText(response.toString());
+                    } else {
+                        text.setText(httpResponse.getStatusLine().toString());
+                    }
+                }
+         catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            public void onClick(View v) {
-                // http postの処理を書く
-            }
-        });
     }
+});
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+View buttonPost = findViewById(R.id.buttonPost);
+buttonPost.setOnClickListener(new OnClickListener() {
+
+    public void onClick(View v) {
+        // http postの処理を書く
     }
+});
+}
+
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+}
 
 }
