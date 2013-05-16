a5a5fee4614169f52ca99c42de80bfd43bb6ac66
Nobuhisa HIRATA <wirapon55@gmail.com>
2013-05-16 15:28:50 +0900

    Complete SQLite practice 1.

--- practice/fundamentals/10th/SQLitePractice/src/jp/mixi/practice/sqlite/MainActivity.java
+++ practice/fundamentals/10th/SQLitePractice/src/jp/mixi/practice/sqlite/MainActivity.java
@@ -2,10 +2,18 @@
 package jp.mixi.practice.sqlite;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.renderscript.Int2;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import jp.mixi.practice.sqlite.BookOpenHelper;
 
 public class MainActivity extends Activity {
 
@@ -13,48 +21,110 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final BookOpenHelper bookOpenHelper = new BookOpenHelper(getApplicationContext());
 
         findViewById(R.id.Insert).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                insert();
+                insert(bookOpenHelper);
             }
         });
         findViewById(R.id.Delete).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                delete();
+                delete(bookOpenHelper);
             }
         });
         findViewById(R.id.Update).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                update();
+                update(bookOpenHelper);
             }
         });
         findViewById(R.id.Query).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                query();
+                query(bookOpenHelper);
             }
         });
 
     }
 
-    private void insert() {
+    private void insert(BookOpenHelper bookOpenHelper) {
         // TODO:ここにinsert処理を実装してください
+        // 書き込み用のSQLiteDatabaseを取得
+        SQLiteDatabase db = bookOpenHelper.getWritableDatabase();
+        
+        ContentValues values = new ContentValues();
+        values.put(Book.COLUMN_NAME_BOOK_TITLE, "TITLE1");
+        values.put(Book.COLUMN_NAME_BOOK_PUBLISHER, "PUBLISHER1");
+        values.put(Book.COLUMN_NAME_BOOK_PRICE, "PRICE1");
+
+        // 戻り値はRowID（_ID）
+        // エラーの場合は-1になる
+        long rowId = db.insert(Book.BOOK_TABLE_NAME, null, values);
+        Toast.makeText(getApplicationContext(), Long.toString(rowId), Toast.LENGTH_SHORT).show();
+        Log.e("insert", Long.toString(rowId));
     }
 
-    private void delete() {
+    private void delete(BookOpenHelper bookOpenHelper) {
         // TODO:ここにdelete処理を実装してください
+        SQLiteDatabase db = bookOpenHelper.getWritableDatabase();
+
+        // 条件を指定
+        String selection = Book.COLUMN_NAME_BOOK_PRICE + " = ?";
+        String[] selectionArgs = {
+                "PRICE1"
+        };
+        int deletedCount = db.delete(Book.BOOK_TABLE_NAME, selection, selectionArgs);
+        Toast.makeText(getApplicationContext(), Integer.toString(deletedCount), Toast.LENGTH_SHORT).show();
+        Log.e("delete", Integer.toString(deletedCount));
     }
 
-    private void update() {
+    private void update(BookOpenHelper bookOpenHelper) {
         // TODO:ここにupdate処理を実装してください
+        SQLiteDatabase db = bookOpenHelper.getWritableDatabase();
+
+        // update情報を設定する
+        ContentValues values = new ContentValues();
+        values.put(Book.COLUMN_NAME_BOOK_TITLE, "NEW_TITLEげえええええええ！？");
+
+        // 条件を指定
+        String selection = Book.COLUMN_NAME_BOOK_TITLE + " LIKE ?";
+        String[] selectionArgs = {
+                "TITLE%"
+        };
+
+        int updatedCount = db.update(Book.BOOK_TABLE_NAME, values, selection, selectionArgs);
+        Toast.makeText(getApplicationContext(), Integer.toString(updatedCount), Toast.LENGTH_SHORT).show();
+        Log.e("update", Integer.toString(updatedCount));
+
     }
 
-    private void query() {
+    private void query(BookOpenHelper bookOpenHelper) {
         // TODO:ここにquery処理を実装してください
+        // 読み込み用のSQLiteDatabaseを取得
+        SQLiteDatabase db = bookOpenHelper.getReadableDatabase();
+
+        // 取得する情報を指定
+        String[] projection = {
+                Book._ID,
+                Book.COLUMN_NAME_BOOK_TITLE,
+                Book.COLUMN_NAME_BOOK_PUBLISHER,
+                Book.COLUMN_NAME_BOOK_PRICE
+        };
+
+        // 条件を指定
+        String selection = Book.COLUMN_NAME_BOOK_PRICE + " = ?";
+        String[] selectionArgs = {
+                "PRICE1"
+        };
+
+        Cursor cursor = db.query(Book.BOOK_TABLE_NAME, projection, selection, selectionArgs, null, null, null);
+        boolean moveToFirst = cursor.moveToFirst();
+        long itemId = cursor.getLong(cursor.getColumnIndexOrThrow(Book._ID));
+        Toast.makeText(getApplicationContext(), Long.toString(itemId), Toast.LENGTH_SHORT).show();
+        Log.e("select", Long.toString(itemId));        
     }
 
     @Override
