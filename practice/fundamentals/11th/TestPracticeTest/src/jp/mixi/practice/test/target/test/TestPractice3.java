61e580b85b4963d919a018a2d312e84e166f029f
Nobuhisa HIRATA <wirapon55@gmail.com>
2013-05-20 19:30:18 +0900

    Fix test practice 3.

--- practice/fundamentals/11th/TestPracticeTest/src/jp/mixi/practice/test/target/test/TestPractice3.java
+++ practice/fundamentals/11th/TestPracticeTest/src/jp/mixi/practice/test/target/test/TestPractice3.java
@@ -2,11 +2,17 @@ package jp.mixi.practice.test.target.test;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
+import com.jayway.android.robotium.solo.Solo;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import jp.mixi.practice.test.target.TestTarget3;
 import jp.mixi.practice.test.target.R;
@@ -23,11 +29,20 @@ public class TestPractice3 extends ActivityInstrumentationTestCase2<TestTarget3>
 
     public void testInitialStatus() throws Exception {
         // getActivity() の呼び出しで、テスト対象の Activity が立ち上がる
+
         Activity activity = getActivity();
-        // UI 操作による View の状態を見るために、View のインスタンスを取り出す
-        TextView title = (TextView) activity.findViewById(R.id.TitleLabel);
-        TextView content = (TextView) activity.findViewById(R.id.ContentLabel);
-        assertEquals(title.getText().toString(), "タイトル");
-        assertEquals(content.getText().toString(), "本文");
+        Solo solo = new Solo(getInstrumentation(), activity);
+
+        TextView titleCounter = (TextView) activity.findViewById(R.id.TitleCounter);
+        EditText titleEditor = (EditText) activity.findViewById(R.id.TitleEditor);
+        TextView contentCounter = (TextView) activity.findViewById(R.id.ContentCounter);
+        EditText contentEditor = (EditText) activity.findViewById(R.id.ContentEditor);
+
+        solo.assertCurrentActivity("TestTarget3 now.", TestTarget3.class);
+        solo.enterText(titleEditor, "うんこ");
+        solo.enterText(contentEditor, "おじさんおじさん");
+        solo.clickOnButton("完了");
+        assertEquals("3 / 10", titleCounter.getText().toString());
+        assertEquals("8 / 10000", contentCounter.getText().toString());
     }
 }
