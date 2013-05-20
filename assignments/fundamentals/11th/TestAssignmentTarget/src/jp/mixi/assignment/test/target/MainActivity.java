16adb52cd4f22963cb11bff0b6b38a264fda74a8
Nobuhisa HIRATA <wirapon55@gmail.com>
2013-05-20 20:53:48 +0900

    Complete test practice 4.

--- assignments/fundamentals/11th/TestAssignmentTarget/src/jp/mixi/assignment/test/target/MainActivity.java
+++ assignments/fundamentals/11th/TestAssignmentTarget/src/jp/mixi/assignment/test/target/MainActivity.java
@@ -2,7 +2,11 @@ package jp.mixi.assignment.test.target;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,27 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button verifyButton = (Button) findViewById(R.id.VerifyButton);
+        EditText editText = (EditText) findViewById(R.id.IdentityEditor);
+        verifyButton.setEnabled(false);
+
+        editText.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count){
+                int length = s.length();
+                if(length < 0 || length > 20){
+                    verifyButton.setEnabled(false);
+                }else{
+                    verifyButton.setEnabled(true);
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+        });
     }
 
     @Override
