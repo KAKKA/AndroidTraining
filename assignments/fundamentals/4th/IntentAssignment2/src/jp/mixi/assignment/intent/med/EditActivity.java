5e0a4daaf8bcfda0c2d75c07e9ca05e23d2f6d20
Nobuhisa HIRATA <wirapon55@gmail.com>
2013-05-21 18:58:49 +0900

    Squashed commit of the following:dev5
    commit 80a60520152d1256776cd2ee00fd02ba5a73b09c
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Tue May 14 18:01:13 2013 +0900
    
        notification practice 4 is already completed.
    
    commit 2c4e38ed55be0020b881b4d0d9525891793d96f7
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Tue May 14 17:52:05 2013 +0900
    
        notification practice 3 is completed.
    
    commit c3154b5189acbecda7feafd54adbffb73e7bec6d
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Tue May 14 17:21:30 2013 +0900
    
        notification practice 1 and 2 are completed.
    
    commit f49b334ebaddff968982a4328ff2e47925787485
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Tue May 14 16:37:44 2013 +0900
    
        intent practice 4 is completed.
    
    commit f45d76d0a0a24084c2ba770ef61e7eae7e201c29
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Tue May 14 11:46:52 2013 +0900
    
        intent practice 3 is completed.
    
    commit ea9acfa522f086ccafa9edfdcb7db1d5401ff37f
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Thu May 9 17:28:14 2013 +0900
    
        intent practice 2 is completed.
    
    commit e96447553d4266e6b26304e27cc273e2e9b1a4da
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Tue May 7 17:47:33 2013 +0900
    
        intent practice 1 is completed.

--- assignments/fundamentals/4th/IntentAssignment2/src/jp/mixi/assignment/intent/med/EditActivity.java
+++ assignments/fundamentals/4th/IntentAssignment2/src/jp/mixi/assignment/intent/med/EditActivity.java
@@ -1,20 +1,26 @@
 package jp.mixi.assignment.intent.med;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class EditActivity extends Activity {
+public class EditActivity extends Activity{    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
-
-        findViewById(R.id.Submit).setOnClickListener(new OnClickListener() {
+        Button button = (Button) findViewById(R.id.Submit);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO ここで、id が MyEdit の EditText からテキストを取得して、結果にセットする
+                TextView editText = (TextView) findViewById(R.id.MyEdit);
+                Intent intent = new Intent();
+                intent.putExtra("editText", editText.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
             }
         });
     }
