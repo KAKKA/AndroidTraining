1ac8a2df54a18de0376fa21d59a54621b6a0fc66
Nobuhisa HIRATA <wirapon55@gmail.com>
2013-05-21 18:58:02 +0900

    Squashed commit of the following:dev3
    commit a47a1682a37ce4102e5e4cb0f38c3f48feaeb1ed
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Thu May 9 14:22:11 2013 +0900
    
        I forgot commit a file...
    
    commit 8857b2e4745b707b3eb547adf75135922ed3a1d3
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Fri Apr 26 17:34:03 2013 +0900
    
        practice 9 is completed. So I have completed all practices!!!!!!!!!!!!!!!!!!!!!
    
    commit a79bec72e17a9f3645da26f423108a7792d2eb9d
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Fri Apr 26 16:33:49 2013 +0900
    
        practice 6 and 8 is already completed.
    
    commit 53f75ae637ce95e54f4b11cb74b770cb672e954a
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Fri Apr 26 16:21:46 2013 +0900
    
        practice 6 and 7 is completed.
    
    commit 1535ee5f282cc826aa4f831b9dd89a74a69b37ca
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Thu Apr 25 18:28:26 2013 +0900
    
        practice 5 completed.
    
    commit 25f369dc54b512a0f27238fd0c549dadbb952ca2
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Tue Apr 23 15:58:46 2013 +0900
    
        practice 3 and 4 completed.
    
    commit 20cbc5be01cac165a621b45781cb850a66071015
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Tue Apr 23 13:27:41 2013 +0900
    
        checnge activity_unko.xml
    
    commit 9a94c316773fc64db8f35030117c8b4eecd1bfba
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Tue Apr 23 13:18:03 2013 +0900
    
        practice2 is completed.
    
    commit b0364c54b4ddd04c3e3a20870a93130d415a56ec
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Tue Apr 23 13:10:50 2013 +0900
    
        practice1 is completed.

--- assignments/fundamentals/2nd/ControllerAssignment1/src/jp/mixi/assignment/controller/beg/MainActivity.java
+++ assignments/fundamentals/2nd/ControllerAssignment1/src/jp/mixi/assignment/controller/beg/MainActivity.java
@@ -4,6 +4,7 @@ package jp.mixi.assignment.controller.beg;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 /**
  * TODO: 課題1
@@ -17,8 +18,10 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+    	
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    	setContentView(R.layout.activity_main);
     }
 
     @Override
@@ -26,5 +29,36 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    @Override
+    public void onStart(){
+    	Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    	super.onStart();
+    }
+    @Override
+    public void onResume(){
+    	Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    	super.onResume();
+    }
+    @Override
+    public void onPause(){
+    	Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    	super.onPause();
+    }
+    @Override
+    public void onStop(){
+    	Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    	super.onStop();
+    }
+    @Override
+    public void onRestart(){
+    	Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    	super.onRestart();
+    }
+    @Override
+    public void onDestroy(){
+    	Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    	super.onDestroy();
     }
 }
--- practice/fundamentals/2nd/FragmentPractice/Practice1/src/jp/mixi/practice/fragment/beg/MainActivity.java
+++ practice/fundamentals/2nd/FragmentPractice/Practice1/src/jp/mixi/practice/fragment/beg/MainActivity.java
@@ -1,23 +1,38 @@
-
 package jp.mixi.practice.fragment.beg;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		FragmentManager manager = getSupportFragmentManager();
+		// FragmentTransaction を開始
+		FragmentTransaction transaction = manager.beginTransaction();
 
+		// FragmentContainer のレイアウトに、MyFragment を割当てる
+		transaction.add(R.id.FragmentContainer, MyFragment.createInstance());
+
+		// 変更を確定して FragmentTransaction を終える
+		transaction.commit();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 }
+
+
