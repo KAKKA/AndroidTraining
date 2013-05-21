3f418dc71522829d8f7757117f5bbd89a164a878
Nobuhisa HIRATA <wirapon55@gmail.com>
2013-05-21 18:59:16 +0900

    Squashed commit of the following:dev6
    commit e8a50fd6786fbab6a913db5502516aab368aeb57
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Wed May 8 15:35:00 2013 +0900
    
        interaction practice 2 is completed.
    
    commit ebbaa84c11439e1911d21a3f7c6897d8a5beb474
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Wed May 8 14:26:40 2013 +0900
    
        interaction practice 1 is completed.
    
    commit f16519973ce6a963721c712d3b87cec622a931eb
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Wed May 8 14:12:23 2013 +0900
    
        action bar practice 3 is completed.
    
    commit 377f721cbd3c9ba99cf6a155e492d3933865fb8b
    Author: Nobuhisa HIRATA <wirapon55@gmail.com>
    Date:   Wed May 8 13:52:26 2013 +0900
    
        action bar practice 1 and 2 are completed.

--- practice/fundamentals/5th/ActionBarPractice1/src/jp/mixi/practice/actionbar/beg/MainActivity.java
+++ practice/fundamentals/5th/ActionBarPractice1/src/jp/mixi/practice/actionbar/beg/MainActivity.java
@@ -1,23 +1,56 @@
-
 package jp.mixi.practice.actionbar.beg;
 
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends SherlockActivity implements TabListener {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+		// タブナビゲーションモードに設定
+		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		// タブを作成して追加。タブの選択・解除・再選択をハンドリングするコールバックの TabListener をセットしないと実行時例外でクラッシュする
+		getSupportActionBar().addTab(getSupportActionBar().newTab().setText("堀田").setTabListener(this));
+		getSupportActionBar().addTab(getSupportActionBar().newTab().setText("佐野").setTabListener(this));
+		getSupportActionBar().addTab(getSupportActionBar().newTab().setText("takeny").setTabListener(this));
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // メニューの要素を追加
+	    getSupportMenuInflater().inflate(R.menu.main, menu);
+	    return true;
+	}
+	 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+	    return true;
+	}
+	// タブナビゲーションの Tab が選択された時のコールバック
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		Toast.makeText(getApplicationContext(), tab.getText(), Toast.LENGTH_SHORT).show();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	}
 
+	// タブナビゲーションの Tab が選択解除された時のコールバック
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+
+	}
+
+	// タブナビゲーションの Tab が再度選択された時のコールバック
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+
+	}
+	
 }
--- practice/fundamentals/5th/InteractionCallbackPractice1/src/jp/mixi/practice/interaction/beg/MainActivity.java
+++ practice/fundamentals/5th/InteractionCallbackPractice1/src/jp/mixi/practice/interaction/beg/MainActivity.java
@@ -4,6 +4,8 @@ package jp.mixi.practice.interaction.beg;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -19,5 +21,15 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // ここで、状態に応じてメニューの有効・無効を切り替えたりなどの処理をする
+        return super.onPrepareOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+    	// 選択されたメニューに対応するイベント処理をここで実行する
+        return super.onOptionsItemSelected(item);
+    }
 }
